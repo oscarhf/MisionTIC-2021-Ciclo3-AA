959ee72d26b86002cfba5d59af0d70934e7a968c
Oscar Hernán Franco Bedoya <oscarhf2002@outlook.com>
2021-10-30 14:57:29 -0500

    Codigo Completo

---
+++ HospiEnCasa.App.Persistencia/AppRepositorios/RepositorioPaciente.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using HospiEnCasa.App.Dominio;
+
+namespace HospiEnCasa.App.Persistencia
+{
+
+    public class RepositorioPaciente : IRepositorioPaciente
+    {
+        /// <summary>
+        /// Referencia al contexto de Paciente
+        /// </summary>
+        private readonly AppContext _appContext;
+        /// <summary>
+        /// Metodo Constructor Utiiza 
+        /// Inyeccion de dependencias para indicar el contexto a utilizar
+        /// </summary>
+        /// <param name="appContext"></param>//
+        public RepositorioPaciente(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+
+        Paciente IRepositorioPaciente.AddPaciente(Paciente paciente)
+        {
+            var pacienteAdicionado = _appContext.Pacientes.Add(paciente);
+            _appContext.SaveChanges();
+            return pacienteAdicionado.Entity;
+
+        }
+
+        void IRepositorioPaciente.DeletePaciente(int idPaciente)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.FirstOrDefault(p => p.Id == idPaciente);
+            if (pacienteEncontrado == null)
+                return;
+            _appContext.Pacientes.Remove(pacienteEncontrado);
+            _appContext.SaveChanges();
+        }
+
+        IEnumerable<Paciente> IRepositorioPaciente.GetAllPacientes()
+        {
+            return _appContext.Pacientes;
+        }
+
+        Paciente IRepositorioPaciente.GetPaciente(int idPaciente)
+        {
+            return _appContext.Pacientes.FirstOrDefault(p => p.Id == idPaciente);
+        }
+
+        Paciente IRepositorioPaciente.UpdatePaciente(Paciente paciente)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.FirstOrDefault(p => p.Id == paciente.Id);
+            if (pacienteEncontrado != null)
+            {
+                pacienteEncontrado.Nombre = paciente.Nombre;
+                pacienteEncontrado.Apellidos = paciente.Apellidos;
+                pacienteEncontrado.NumeroTelefono = paciente.NumeroTelefono;
+                pacienteEncontrado.Genero = paciente.Genero;
+                pacienteEncontrado.Direccion = paciente.Direccion;
+                pacienteEncontrado.Latitud = paciente.Latitud;
+                pacienteEncontrado.Longitud = paciente.Longitud;
+                pacienteEncontrado.Ciudad = paciente.Ciudad;
+                pacienteEncontrado.FechaNacimiento = paciente.FechaNacimiento;
+                pacienteEncontrado.Familiar = paciente.Familiar;
+                pacienteEncontrado.Enfermera = paciente.Enfermera;
+                pacienteEncontrado.Medico = paciente.Medico;
+                pacienteEncontrado.Historia = paciente.Historia;
+
+                _appContext.SaveChanges();
+
+
+            }
+            return pacienteEncontrado;
+        }
+
+        Medico IRepositorioPaciente.AsignarMedico(int idPaciente, int idMedico)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.FirstOrDefault(p => p.Id == idPaciente);
+            if (pacienteEncontrado != null)
+            {
+                var medicoEncontrado = _appContext.Medicos.FirstOrDefault(m => m.Id == idMedico);
+                if (medicoEncontrado != null)
+                {
+                    pacienteEncontrado.Medico = medicoEncontrado;
+                    _appContext.SaveChanges();
+                }
+                return medicoEncontrado;
+            }
+            return null;
+        }
+
+        IEnumerable<Paciente> IRepositorioPaciente.GetPacientesMasculinos()
+        {
+
+            return _appContext.Pacientes.Where(p => p.Genero == Genero.Masculino).ToList();
+
+        }
+
+        public IEnumerable<Paciente> GetPacientesCorazon()
+        {
+
+            return _appContext.Pacientes
+                                   .Where(p => p.SignosVitales.Any(s =>  TipoSigno.FrecuenciaCardica==s.Signo && s.Valor >=90))
+                                   .ToList();
+        } 
+    }
+}
